9f35f1a1c951c95004c9eb765ecf87c68643fa4e
Eson Paguia <etpaguia@gmail.com>
2021-07-23 05:04:39 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Admin Feature
+  Engagement API for Admin Management
+
+## Developer setup
+```
+npm install
+npm start
+```
---
+++ routes/features.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.json(
+    {
+      "message" : "Get resource"
+    }
+  );
+});
+
+router.post('/', function(req, res, next) {
+  res.json(
+    {
+      "message" : "Post resource"
+    }
+  );
+});
+
+router.patch('/', function(req, res, next) {
+  res.json(
+    {
+      "message" : "Patch resource"
+    }
+  );
+});
+
+router.delete('/', function(req, res, next) {
+  res.json(
+    {
+      "message" : "Delete resource"
+    }
+  );
+});
+
+module.exports = router;
