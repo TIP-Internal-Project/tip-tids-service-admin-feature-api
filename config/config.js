c80a5fcaf06be7d91d612bf2a58bbaf75eb5740d
rjsarellanoti <ryan.arellano@telusinternational.com>
2023-03-27 20:50:42 +0800

    03/27/2023

--- config/config.js
+++ config/config.js
@@ -1,14 +1,11 @@
 module.exports = {
     databaseUri: 'mongodb://api_user:api1234@127.0.0.1:27017/tip'
     
-    
-    
+    //test
     // 'mongodb+srv://kkapangyarihan:PY2fc5VVaDFvx6m5@cluster0.zhuuyag.mongodb.net/?retryWrites=true&w=majority'
     
     
-    
     // 'mongodb+srv://rjsarellanoti:ZJdPRyBjVPmZ1PIO@cluster0.hdzyevf.mongodb.net/?retryWrites=true&w=majority'
-
 
     //  databaseUri: 'mongodb://api_user:api1234@127.0.0.1:27017/tip'
 }
