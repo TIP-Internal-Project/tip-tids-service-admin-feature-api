b984c6b95005fceaa811018dc54dd8af25d37744
adolphmagante18 <adolphedsue.magante@telus.com>
2024-07-22 20:01:39 +0800

    fix: ScheduleJobs every 5mins

--- src/routes/EventsRoute.js
+++ src/routes/EventsRoute.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const eventsController = require("../controllers/EventsController");
-const multer = require("multer");
 const upload = require("../middleware/UploadMiddleware");
 
 router.get("/getAllEvents", eventsController.getAllEvents);
--- src/schedule/ScheduleJobs.js
+++ src/schedule/ScheduleJobs.js
@@ -2,7 +2,7 @@ const schedule = require("node-schedule");
 const Event = require("../models/Event");
 
 module.exports = function ScheduleJobs() {
-  schedule.scheduleJob("*/1 * * * *", async () => {
+  schedule.scheduleJob("*/5 * * * *", async () => {
     const now = new Date();
     const outdatedEvents = await Event.find({
       endDate: { $lt: now },
@@ -10,7 +10,9 @@ module.exports = function ScheduleJobs() {
     });
 
     for (const event of outdatedEvents) {
-      if (new Date(event.endDate.setHours(event.endDate.getHours())) <= now) {
+      if (
+        new Date(event.endDate.setHours(event.endDate.getHours() + 12)) <= now
+      ) {
         event.status = "Completed";
         await event.save();
         console.log("Event ID " + event.eventId + " is Completed");
