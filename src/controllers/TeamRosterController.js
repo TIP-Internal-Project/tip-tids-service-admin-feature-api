75b63d4ecac9f2de1b5326f86c62e7f2352800c2
adolphmagante18 <adolphedsue.magante@telus.com>
2024-07-24 20:31:09 +0800

    cleanUp routers

--- src/controllers/TeamRosterController.js
+++ src/controllers/TeamRosterController.js
@@ -6,13 +6,29 @@ const getAllTeamMember = async (req, res) => {
 };
 
 const getTeamMemberInfoByEmail = async (req, res) => {
-  const teamMember = await TeamRosterService.getTeamMemberInfoByEmail(
-    req.params.workEmailAddress
-  );
-  if (teamMember.length > 0) {
-    return res.send(teamMember);
+  const { workEmailAddress } = req.params;
+
+  // Check if the email parameter is missing or invalid
+  if (!workEmailAddress || !workEmailAddress.includes("@")) {
+    return res
+      .status(400)
+      .json({ message: "Invalid or missing email parameter" });
   }
-  res.status(404).json({ message: "not found" });
+
+  try {
+    const teamMember = await TeamRosterService.getTeamMemberInfoByEmail(
+      workEmailAddress
+    );
+
+    if (teamMember.length > 0) {
+      return res.send(teamMember);
+    } else {
+      return res.status(404).json({ message: "Team Member not found" });
+    }
+  } catch (error) {
+    // Optionally handle any other errors that might occur
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 module.exports = { getAllTeamMember, getTeamMemberInfoByEmail };
--- src/routes/OverviewRoute.js
+++ src/routes/OverviewRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const overviewController = require("../controllers/overviewController");
+const overviewController = require("../controllers/OverviewController");
 
 router.get("/", function (req, res, next) {
   res.render("overview", { title: "Overview API" });
